56476f3315f002b838421431f227d23ae5729a82
TehKeeper <lexmechanicum@gmail.com>
2019-11-09 13:09:16 +0300

    TK-1

---
+++ Assets/Canvas_Script.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;/using UnityEngine.UI;
+
+public class Canvas_Script : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Text 
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
